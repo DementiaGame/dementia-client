42be5e2d44972147ad1ca228b431892e652a0c93
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-06-25 19:20:05 +0900

    feat: 렌더링 되도록 router 설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import InitialGame from "./components/game/initialgame/InitialGame";
+import InitialGameQuestions from "./components/game/initialgame/InitialGameQuestions";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<InitialGame />} />
+        <Route
+          path="/questions/:topicName/:userId"
+          element={<InitialGameQuestions />}
+        />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
