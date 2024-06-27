102e37154bbe9c10d8efea1ed4eea3efe7cf0dba
이 다정 <vicpillon0319@gamil.com>
2024-06-28 01:56:20 +0900

    feat: app router 등록

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GameMain from "./components/game/GameMain";
 import InitialGame from "./components/game/initialgame/InitialGame";
 import InitialGameQuestions from "./components/game/initialgame/InitialGameQuestions";
+import SignIn from "./user/SignIn";
+import BirthdayStep from "./user/BirthyearStep";
+import GenderStep from "./user/GenderStep";
+import NickNameStep from "./user/nicknameStep";
+import PasswordStep from "./user/PasswordStep";
+import CompleteSignUp from "./user/CompleteSignup";
 
 function App() {
   return (
@@ -14,6 +20,12 @@ function App() {
           path="/questions/:topicName/:userId"
           element={<InitialGameQuestions />}
         />
+        <Route path="/signin" element={<SignIn />}/>
+        <Route path="/signup/birthyear-step" element={<BirthdayStep />}/>
+        <Route path="/signup/gender-step" element={<GenderStep />}/>
+        <Route path="/signup/nickname-step" element={<NickNameStep />}/>
+        <Route path="/signup/password-step" element={<PasswordStep />}/>
+        <Route path="/signup/complete-signup" element={<CompleteSignUp />}/>
       </Routes>
     </Router>
   );
