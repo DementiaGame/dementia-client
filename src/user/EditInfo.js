fc38e493452bbff4d7d5dc7815b9658bb92115b8
vicpillon <dew0489@naver.com>
2024-07-07 21:11:18 +0900

    feat: 사용자 정보 수정 페이지 생성

---
+++ src/user/EditInfo.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { FaCog, FaUserCircle } from "react-icons/fa";
+import { MdChevronRight } from 'react-icons/md';
+import { useSetRecoilState } from "recoil";
+import { userState } from "../recoil/userState";
+import "./EditInfo.css";
+
+const EditInfo = () => {
+}
+
+export default EditInfo;
